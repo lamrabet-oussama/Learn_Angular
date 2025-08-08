fdb8b37139dc28435e6550d811b219fb326223e0
lamrabet-oussama <lamrabetoussama184@gmail.com>
2025-08-08 12:13:37 +0200

    create reducers

---
+++ learn/src/app/books/book.reducers.ts
@@ -0,0 +1,11 @@
+import { createReducer, on } from "@ngrx/store";
+import { Book } from "../models/book";
+import { AddBook, RemoveBook } from "./book.actions";
+
+
+export const initialState:ReadonlyArray<Book>=[];
+export const BookReducer=createReducer(
+    initialState,
+    on(AddBook,(state,{id,title,author})=>[...state,{id,title,author}]),
+    on(RemoveBook,(state,{bookId})=>state.filter(book=>book.id!==bookId))
+);
